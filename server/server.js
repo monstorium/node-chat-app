a1f9c2eb775f259bbdea6703104b018a2d08c378
vgiridhar <vgiridhar3@gatech.edu>
2017-07-20 17:20:15 -0400

    Greet new user

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-const port = 8080;
+const port = process.env.port || 3000;
 
 var app = express();
 var server = http.createServer(app);
@@ -15,9 +15,27 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	socket.emit('newMessage', {
+		from: "Admin",
+		text: "Welcome to the Chat App",
+		createdAt: new Date().getTime()
+	});
+
+	socket.broadcast.emit('newMessage',{
+		from: "Admin",
+		text: "New User Joined",
+		createdAt: new Date().getTime()
+	});
+
 	socket.on('createMessage', (message) => {
 		console.log('createMessage', message);
 		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		});
+
+		socket.broadcast.emit('newMessage',{
 			from: message.from,
 			text: message.text,
 			createdAt: new Date().getTime()
